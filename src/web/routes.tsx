123d3d7e90ca2f8134bc8e5babedead490616ce8
Mohamed Telli <tellimohamed91@gmail.com>
2024-12-07 20:05:09 +0100

    Teacher : class average page final version (averages page and update routing; include new dependencies)

--- src/web/routes.tsx
+++ src/web/routes.tsx
@@ -1,14 +1,18 @@
 // src/web/routes.tsx
 import { Routes, Route } from 'react-router-dom';
 import { Grades } from './pages/Grades';
-import { TeacherGrades } from './pages/teacher-grades';
+import { TeacherGrades } from './pages/TeacherGrades';
+import { Averages } from './pages/Averages';
+import { Admin } from './pages/Admin';
+
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/grades" element={<Grades />} />
       <Route path="/teacher-grades" element={<TeacherGrades />} />
-      {/* Ajoutez d'autres routes ici */}
+      <Route path="/averages" element={<Averages />} />
+      <Route path="/admin" element={<Admin />} />
     </Routes>
   );
 }
