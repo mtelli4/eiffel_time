2ccae4a4bd248be0714e454a08b7c6448f8e00f4
Mohamed Telli <tellimohamed91@gmail.com>
2024-12-28 12:15:36 +0100

    Initialize mobile app structure; add HomeScreen and MobileButton components, configure TypeScript, and update project settings

--- packages/shared/src/components/Teacher/AddGradeModal.tsx
+++ packages/shared/src/components/Teacher/AddGradeModal.tsx
@@ -1,7 +1,7 @@
 import { Platform } from 'react-native'
-import { MobileAddGradeModal } from '../../../../mobile/src/components/AddGradeModal.native'
-import type { AddGradeModalProps } from '../../types/types'
+import { MobileAddGradeModal } from '../../../../../src/components/AddGradeModal.native'
 import { WebAddGradeModal } from '../../../../web/src/components/AddGradeModal.web'
+import type { AddGradeModalProps } from '../../types/types'
 
 export function AddGradeModal(props: AddGradeModalProps) {
   if (Platform.OS === 'web') {
