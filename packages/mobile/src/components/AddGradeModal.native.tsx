111f257428f2729835325db12d9811d9bb090e0e
Mohamed TELLI <tellimohamed91@gmail.com>
2025-01-02 14:17:44 +0100

    Refactor AddGradeModal styles into a separate file for better organization

--- packages/mobile/src/components/AddGradeModal.native.tsx
+++ packages/mobile/src/components/AddGradeModal.native.tsx
@@ -1,14 +1,7 @@
 import {useState} from 'react';
-import {
-  Button,
-  Modal,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import {Button, Modal, ScrollView, Text, TextInput, View} from 'react-native';
 import {AddGradeModalProps, FormData} from '../../../shared/src/types/types';
+import {styles} from '../styles/AddGradeModal';
 
 export function MobileAddGradeModal({
   isOpen,
@@ -101,50 +94,3 @@ export function MobileAddGradeModal({
     </Modal>
   );
 }
-
-const styles = StyleSheet.create({
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    borderRadius: 10,
-    width: '90%',
-    padding: 20,
-    maxHeight: '80%',
-  },
-  modalHeader: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  modalTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  formContainer: {
-    maxHeight: '70%',
-  },
-  formGroup: {
-    marginBottom: 20,
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    padding: 10,
-  },
-  modalFooter: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 20,
-  },
-});
