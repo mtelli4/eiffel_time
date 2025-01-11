ad092b6c19ef4a27a4685b9ee772429160f60081
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-01-11 20:08:09 +0100

    Améliore la configuration du serveur Express
    Ajoute une configuration plus spécifique pour CORS, en précisant les origines et méthodes autorisées. Préfixe les routes avec "/api" et ajoute un middleware pour analyser le corps des requêtes au format JSON.

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,16 @@ const routes = require('./routes');  // Import des routes définies dans routes.
 
 const app = express();
 
-// Middleware pour permettre les requêtes depuis n'importe quel domaine (CORS)
-app.use(cors());
+app.use(cors({
+    origin: '*', // Accepter toutes les origines
+    methods: 'GET,POST,PUT,DELETE', // Méthodes autorisées
+}));
+
+// Middleware pour analyser le corps des requêtes en JSON
+app.use(express.json());
 
 // Intégration des routes dans l'application Express
-app.use(routes);
+app.use('/api', routes);  // Assurez-vous que les routes sont préfixées par "/api"
 
 // Lancer le serveur sur le port 4000
 app.listen(4000, () => {
