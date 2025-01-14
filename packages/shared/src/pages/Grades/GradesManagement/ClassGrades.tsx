37319f10793c9ade30a704b17bb2404211577585
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-01-14 17:10:51 +0100

    Ajouter la propriété 'cours' dans le composant ClassGrades et améliorer la gestion des modales d'ajout de notes pour le web et mobile

--- packages/shared/src/pages/Grades/GradesManagement/ClassGrades.tsx
+++ packages/shared/src/pages/Grades/GradesManagement/ClassGrades.tsx
@@ -154,6 +154,7 @@ export function ClassGrades() {
                     }}
                     modules={modules}
                     students={[]}
+                    cours={cours}
                 />
             )}
         </View>
