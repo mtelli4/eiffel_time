31fd1cd8f812105e58c2cf4369e8e714924481aa
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-01-14 12:41:36 +0100

    Ajouter la méthode getPeriodeName pour retourner le nom de la période d'évaluation

--- backend/classes/Evaluation.ts
+++ backend/classes/Evaluation.ts
@@ -28,6 +28,18 @@ export class Evaluation {
         return periode[this.evaluation.periode as keyof typeof periode] || 'Période non définie';
     }
 
+    getPeriodeName(): string {
+        switch (this.evaluation.periode) {
+            case periode.Semestre_1: return 'Semestre 1';
+            case periode.Semestre_2: return 'Semestre 2';
+            case periode.Semestre_3: return 'Semestre 3';
+            case periode.Semestre_4: return 'Semestre 4';
+            case periode.Semestre_5: return 'Semestre 5';
+            case periode.Semestre_6: return 'Semestre 6';
+            default: return 'Indéfini';
+        }
+    }
+
     getCreatedAt(): Date {
         return this.evaluation.createdat || new Date();
     }
