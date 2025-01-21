1a6164a8801a94747b6bcf18ebccddbb36c66961
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-01-21 20:55:26 +0100

    Simplification de l'affichage du statut des enseignants dans le tableau des utilisateurs

--- packages/web/src/components/Admin/UserTable.web.tsx
+++ packages/web/src/components/Admin/UserTable.web.tsx
@@ -198,16 +198,19 @@ export function UserTable({ users, isAdmin, onEdit, onDelete, roleSelected, grou
                                     .join(', ') || '-'}
                             </td>
                             <td className="py-3 px-4">
-                                {enseignants
-                                    .filter(
-                                        (e) =>
-                                            e.getId() ===
-                                            user.getId()
-                                    )
-                                    .map((e) => {
-                                        return e.isVacataire() ? 'Vacataire' : 'Titulaire'
-                                    })
-                                    .join(', ') || '-'}
+                                {
+                                    /* isVacataire of the actual user */
+                                    enseignants
+                                        .filter(
+                                            (e) =>
+                                                e.getId() === user.getId()
+                                        )
+                                        .map((e) =>
+                                            e.isVacataire()
+                                                ? 'Vacataire'
+                                                : 'Titulaire'
+                                        )[0] || '-'
+                                }
                             </td>
                             <td className="py-3 px-4">
                                 <div className="flex justify-end gap-2">
