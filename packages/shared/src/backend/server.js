2fac3478de3fdf98383233dc2ddd2fecad5875f4
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-01-23 20:46:46 +0100

    Remplacement de la configuration CORS par un middleware personnalisé pour un contrôle plus granulaire des en-têtes et des méthodes autorisées

--- packages/shared/src/backend/server.js
+++ packages/shared/src/backend/server.js
@@ -11,7 +11,14 @@ const corsOptions = {
   credentials: true, // Nécessaire pour les requêtes avec des cookies ou des sessions
 }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 // Middleware pour analyser le corps des requêtes en JSON
 app.use(express.json());
