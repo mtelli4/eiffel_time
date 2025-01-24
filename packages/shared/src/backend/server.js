1a5301f636184236a330b8bf67861aba3e225145
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-01-24 22:34:00 +0100

    Amélioration de la lisibilité du middleware CORS dans le serveur, en ajustant l'indentation du code.

--- packages/shared/src/backend/server.js
+++ packages/shared/src/backend/server.js
@@ -14,10 +14,10 @@ const corsOptions = {
 // app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 // Middleware pour analyser le corps des requêtes en JSON
