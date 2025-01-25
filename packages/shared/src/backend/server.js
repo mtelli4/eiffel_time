0354c6430fd69bf829344e089e950b4a3852c49a
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-01-25 18:43:29 +0100

    Activation de CORS avec les options configurées dans le middleware

--- packages/shared/src/backend/server.js
+++ packages/shared/src/backend/server.js
@@ -11,14 +11,7 @@ const corsOptions = {
   credentials: true, // Nécessaire pour les requêtes avec des cookies ou des sessions
 }
 
-// app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors(corsOptions));
 
 // Middleware pour analyser le corps des requêtes en JSON
 app.use(express.json());
