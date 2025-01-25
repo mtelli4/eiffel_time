21ca09d9d691ab45599fbc5e4d5e04bfa5dcd77a
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-01-25 18:43:25 +0100

    Ajout de la route pour mettre à jour un utilisateur et suppression de l'ancien script de mise à jour.

--- packages/shared/src/backend/routes.js
+++ packages/shared/src/backend/routes.js
@@ -429,4 +429,21 @@ router.post('/insert-evaluation', async (req, res) => {
   }
 });
 
+// Route pour mettre à jour un utilisateur
+router.put('/update-user/:id', async (req, res) => {
+  const { id } = req.params;
+  const { nom, prenom, email, statut } = req.body;
+
+  try {
+      res.json(await prisma.$transaction(async (prisma) => {
+          return await prisma.utilisateur.update({
+              where: { id: parseInt(id) },
+              data: { nom, prenom, email, statut }
+          })
+      }));
+  } catch (error) {
+      res.status(500).json({ error: "Impossible de mettre à jour l'utilisateur ${id}" });
+  }
+});
+
 module.exports = router;  // Exporte le routeur pour l'utiliser dans d'autres fichiers
