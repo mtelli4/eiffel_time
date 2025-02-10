400f311b5931bdc6c564c01cfcae5000dbab51c8
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-10 22:19:08 +0100

    Suppression de la gestion des routes pour les produits dans le routeur principal

--- packages/shared/src/backend/routes/index.js
+++ packages/shared/src/backend/routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 
 const usersRouter = require('./login');
-const productsRouter = require('./products');
 
 const router = express.Router();
 
 // Utilisation des routes
 router.use('/login', usersRouter);
-router.use('/products', productsRouter);
 
 module.exports = router;
