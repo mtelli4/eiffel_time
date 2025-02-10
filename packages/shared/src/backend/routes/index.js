bf62023c52b4bdc796750191b2cdef78a634cd21
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-10 23:08:38 +0100

    Ajout de la gestion des utilisateurs avec des routes pour créer, mettre à jour, récupérer et supprimer des utilisateurs

--- packages/shared/src/backend/routes/index.js
+++ packages/shared/src/backend/routes/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 
+const allRouter = require('./all');
 const dataRouter = require('./data');
 const usersRouter = require('./login');
 const evaluationRouter = require('./evaluation');
+const adminRouter = require('./admin');
 
 const router = express.Router();
 
 // Utilisation des routes
+router.use('/all', allRouter);
 router.use('/data', dataRouter);
 router.use('/login', usersRouter);
 router.use('/evaluation', evaluationRouter);
+router.use('/admin', adminRouter);
 
 module.exports = router;
