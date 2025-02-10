1672ef9c3148244262f63015768f8b8110e86fb3
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-10 22:21:50 +0100

    Ajout de la gestion des routes pour récupérer l'ensemble des données via une nouvelle route dédiée

--- packages/shared/src/backend/routes/index.js
+++ packages/shared/src/backend/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
+const dataRouter = require('./data');
 const usersRouter = require('./login');
 
 const router = express.Router();
 
 // Utilisation des routes
+router.use('/data', dataRouter);
 router.use('/login', usersRouter);
 
 module.exports = router;
