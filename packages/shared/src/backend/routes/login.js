5e73721508005113215740557b7f44f8e3c020fc
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-10 22:18:04 +0100

    Ajout des routes pour la gestion des utilisateurs et des produits, ainsi que la configuration du serveur pour utiliser un port dynamique

---
+++ packages/shared/src/backend/routes/login.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await prisma.utilisateur.findUnique({
+      where: {
+        email: req.body.email,
+      }
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs' });
+  }
+});
