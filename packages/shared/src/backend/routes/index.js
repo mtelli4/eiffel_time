1b9317a7526d31baeeeedb638105960079393dce
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-11 11:26:57 +0100

    Ajout de la gestion des présences des enseignants et réorganisation des routes API

--- packages/shared/src/backend/routes/index.js
+++ packages/shared/src/backend/routes/index.js
@@ -1,18 +1,20 @@
 const express = require('express');
 
-const allRouter = require('./all');
 const dataRouter = require('./data');
 const usersRouter = require('./login');
+const allRouter = require('./all');
 const evaluationRouter = require('./evaluation');
+const teacherAttendanceRouter = require('./teacher-attendance');
 const adminRouter = require('./admin');
 
 const router = express.Router();
 
 // Utilisation des routes
-router.use('/all', allRouter);
 router.use('/data', dataRouter);
 router.use('/login', usersRouter);
+router.use('/all', allRouter);
 router.use('/evaluation', evaluationRouter);
+router.use('/teacher-attendance', teacherAttendanceRouter);
 router.use('/admin', adminRouter);
 
 module.exports = router;
