675d0a553923c7a47fcdf206419c2f4cd696e22f
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-12 09:41:44 +0100

    Ajout d'une route pour la page des absences

---
+++ packages/shared/src/backend/routes/absence.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+// Route pour récupérer toutes les absences
+router.get('/select', async (req, res) => {
+  const absences = await prisma.absence.findMany({
+    select: {
+      id_absence: true,
+      etudiant: {
+        select: {
+          utilisateur: {
+            select: {
+              nom: true,
+              prenom: true,
+            }
+          }
+        }
+      },
+      cours: {
+        select: {
+          groupe: {
+            select: {
+              id_grp: true,
+              libelle: true,
+            }
+          },
+          module: {
+            select: {
+              codeapogee: true,
+              libelle: true,
+            }
+          },
+          debut: true,
+        }
+      },
+      updatedat: true,
+      valide: true,
+      envoye: true,
+    },
+    orderBy: {
+      createdat: 'asc',  // Tri par date de création
+    }
+  });
+  res.json(absences);
+});
+
+module.exports = router;
