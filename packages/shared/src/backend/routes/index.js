59255252b40497ef108ac8fafa88b214dda26729
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-12 09:42:05 +0100

    Ajout de la route pour la gestion des absences

--- packages/shared/src/backend/routes/index.js
+++ packages/shared/src/backend/routes/index.js
@@ -4,6 +4,7 @@ const dataRouter = require('./data');
 const usersRouter = require('./login');
 const allRouter = require('./all');
 const evaluationRouter = require('./evaluation');
+const absenceRouter = require('./absence');
 const teacherAttendanceRouter = require('./teacher-attendance');
 const adminRouter = require('./admin');
 
@@ -14,6 +15,7 @@ router.use('/data', dataRouter);
 router.use('/login', usersRouter);
 router.use('/all', allRouter);
 router.use('/evaluation', evaluationRouter);
+router.use('/absence', absenceRouter);
 router.use('/teacher-attendance', teacherAttendanceRouter);
 router.use('/admin', adminRouter);
 
