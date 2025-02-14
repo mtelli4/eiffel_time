e31216aabdb0986929509bb5cff4370e07e75fc8
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-14 09:13:00 +0100

    Ajout de la sélection du format de date dans les paramètres de personnalisation

--- packages/web/src/components/Settings/PersonalizationSettings.web.tsx
+++ packages/web/src/components/Settings/PersonalizationSettings.web.tsx
@@ -1,6 +1,34 @@
 import { Palette } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import Select from 'react-select'
+
+const dateSelectOptions: { value: string; label: string }[] = [
+  { value: 'fr', label: 'DD/MM/YYYY' },
+  { value: 'en', label: 'MM/DD/YYYY' },
+  { value: 'iso', label: 'YYYY-MM-DD' },
+]
+
+const dateFormat = {
+  fr: 'DD/MM/YYYY',
+  en: 'MM/DD/YYYY',
+  iso: 'YYYY-MM-DD',
+}
 
 export default function PersonalizationSettings() {
+  const [date, setDate] = useState(sessionStorage.getItem('dateFormat') || dateFormat.fr)
+
+  const handleDateChange = (newFormat: string) => {
+    sessionStorage.setItem('dateFormat', newFormat)
+    window.dispatchEvent(new Event("dateChange")); // Notifie les autres composants
+    setDate(newFormat); // Met à jour l'état local pour forcer le rendu
+  }
+
+  useEffect(() => {
+    if (!sessionStorage.getItem('dateFormat')) {
+      sessionStorage.setItem('dateFormat', dateFormat.fr)
+    }
+  })
+
   return (
     <div className="bg-white rounded-lg shadow-sm p-6">
       <div className="flex items-center gap-2 mb-4">
@@ -33,9 +61,9 @@ export default function PersonalizationSettings() {
             Format de date
           </label>
           <select className="w-full rounded-lg border-gray-200 focus:ring-secondary focus:border-secondary">
-            <option value="fr">DD/MM/YYYY</option>
-            <option value="en">MM/DD/YYYY</option>
-            <option value="iso">YYYY-MM-DD</option>
+            <option value="fr" onClick={() => handleDateChange('fr')}>DD/MM/YYYY</option>
+            <option value="en" onClick={() => handleDateChange('en')}>MM/DD/YYYY</option>
+            <option value="iso" onClick={() => handleDateChange('iso')}>YYYY-MM-DD</option>
           </select>
         </div>
       </div>
