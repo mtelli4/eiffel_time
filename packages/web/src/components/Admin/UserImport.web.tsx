1fb43593cbc720b835144e8165458843c71dd670
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-18 19:23:44 +0100

    Ajout de la fonctionnalité d'importation d'utilisateurs avec support pour les fichiers CSV et XLSX, et affichage des données dans un tableau.

--- packages/web/src/components/Admin/UserImport.web.tsx
+++ packages/web/src/components/Admin/UserImport.web.tsx
@@ -2,19 +2,63 @@ import { Text, TouchableOpacity, View } from 'react-native'
 import { Utilisateur } from '../../../../shared/src/types/types'
 import { styles } from '../../../../shared/src/styles/Admin/AdminStyles'
 import { FileUp } from 'lucide-react'
+import Papa from 'papaparse'
+import * as XLSX from 'xlsx'
+import { useState } from 'react'
+import DT from 'datatables.net-dt'
+import 'datatables.net-dt/js/dataTables.dataTables.js'
+import DataTable from 'datatables.net-react'
+import '../../styles/dataTables.dataTables.min.css'
+import { roleFinder } from '../../../../shared/src/lib/utils'
+
+DataTable.use(DT);
 
 interface UserImportProps {
   users: Utilisateur[]
 }
 
 export function UserImport({ users }: UserImportProps) {
+  const [jsonData, setJsonData] = useState<any[]>([])
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
-    if (file) {
-      // Traitez le fichier ici
-      console.log('Fichier sélectionné:', file)
+    if (!file) return
+
+    const fileExtension = (file.name.split('.').pop()?.toLowerCase() || '')
+
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const data = e.target?.result
+
+      if (fileExtension === 'csv') {
+        const parsedData = Papa.parse(data, {
+          header: true,
+          skipEmptyLines: true,
+        })
+        setJsonData(parsedData.data)
+      } else if (fileExtension === 'xlsx' || fileExtension === 'xls') {
+        const workbook = XLSX.read(data, { type: 'binary' })
+        const sheetName = workbook.SheetNames[0]
+        const worksheet = workbook.Sheets[sheetName]
+        const parsedData = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
+        setJsonData(parsedData)
+      } else {
+        alert('Format de fichier non pris en charge')
+      }
+    }
+
+    if (fileExtension === 'csv') {
+      reader.readAsText(file)
+    } else {
+      reader.readAsBinaryString(file)
     }
   }
+
+  const colonnes = [
+    { title: 'Nom', data: 'nom' },
+    { title: 'Prénom', data: 'prenom' },
+    { title: 'Email', data: 'email' },
+    { title: 'Rôle', data: 'statut' },
+  ]
 
   return (
     <View style={styles.content}>
@@ -37,7 +81,7 @@ export function UserImport({ users }: UserImportProps) {
           onChange={handleFileUpload}
         />
         <TouchableOpacity
-          onPress={() => {}}
+          onPress={() => { }}
           style={styles.addButton}
         >
           <Text style={styles.addButtonText}>
@@ -45,7 +89,7 @@ export function UserImport({ users }: UserImportProps) {
           </Text>
         </TouchableOpacity>
         <TouchableOpacity
-          onPress={() => {}}
+          onPress={() => { }}
           style={styles.addButton}
         >
           <Text style={styles.addButtonText}>
@@ -53,6 +97,37 @@ export function UserImport({ users }: UserImportProps) {
           </Text>
         </TouchableOpacity>
       </View>
+      <Text>Cette section permet d'importer des utilisateurs afin de les créer, veuillez noter qu'une vérification et une validation des données importées est nécessaire.</Text>
+      <Text>Modalités d'importation des utilisateurs :</Text>
+      <ul className="list-disc list-inside ml-4">
+        <li>Le fichier doit être au format CSV ou XLSX.</li>
+        <li>Les colonnes du fichier doivent être au format suivant : nom (Nom), prenom (Prénom), email (Email), statut (Rôle)</li>
+        <li>Les rôles possibles sont : indefinite (Indéfini), student (Étudiant), teacher (Enseignant), secretary (Secrétaire), director (Directeur).</li>
+        <li>Les utilisateurs seront créés.</li>
+      </ul>
+      <br />
+      {jsonData.length > 0 && (
+        <div className="bg-white rounded-lg shadow-sm overflow-hidden">
+          <DataTable
+            data={jsonData.map((user) => ({
+              nom: user.nom,
+              prenom: user.prenom,
+              email: user.email,
+              statut: roleFinder(user.statut),
+            }))}
+            columns={colonnes}
+            options={{
+              info: true,
+              language: {
+                info: 'Affichage de _START_ à _END_ sur _TOTAL_ utilisateurs',
+                emptyTable: 'Aucun utilisateur trouvé',
+                lengthMenu: 'Afficher _MENU_ utilisateurs',
+                search: 'Rechercher :',
+              },
+            }}
+          />
+        </div>
+      )}
     </View>
   )
 }
