b1826e1e61bf00c9dd29801294a4f83c12e88244
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-27 19:18:00 +0100

    Ajout de l'ajout, suppression d'une évaluation en web ou mobile

--- packages/mobile/src/components/Grades/GradesManagement/AddGradeModal.native.tsx
+++ packages/mobile/src/components/Grades/GradesManagement/AddGradeModal.native.tsx
@@ -1,35 +1,151 @@
-// import {Picker} from '@react-native-picker/picker';
-import {useState} from 'react';
-import {Button, Modal, ScrollView, Text, TextInput, View} from 'react-native';
-import {
-  AddGradeModalProps,
-  FormData,
-} from '../../../../../shared/src/types/types';
-import {styles} from '../../../styles/Grades/GradesManagement/AddGradeModal';
+import { useState } from 'react';
+import { Alert, Button, Modal, Text, TextInput, View, FlatList, KeyboardAvoidingView, Platform, TouchableOpacity } from 'react-native';
+import { AddGradeModalProps, FormEvaluation } from '../../../../../shared/src/types/types';
+import { styles } from '../../../styles/Grades/GradesManagement/AddGradeModal';
+import { dateFormatting } from '../../../../../shared/src/utils/stringUtils';
+import DropDownPicker from 'react-native-dropdown-picker';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 
-export default function MobileAddGradeModal({
-  isOpen,
-  onClose,
-  modules,
-  cours,
-}: AddGradeModalProps) {
-  const [formData, setFormData] = useState<FormData>({
-    moduleId: '',
-    name: '',
-    date: new Date().toISOString().split('T')[0],
-    maxValue: 20,
+export default function MobileAddGradeModal({ isOpen, onClose, onSubmit, module, cours, }: AddGradeModalProps) {
+  const [formData, setFormData] = useState<FormEvaluation>({
+    libelle: '',
     coefficient: 1,
-    grades: students.map(student => ({
-      studentId: student.id,
-      value: null,
-      status: 'ungraded',
-    })),
+    notemaximale: 20,
+    periode: 'Semestre_1',
+    id_cours: 0,
+    id_module: module.id_module,
   });
 
+  const [openCourse, setOpenCourse] = useState(false);
+  const [valueCourse, setValueCourse] = useState(formData.id_cours);
+  const [itemsCourse, setItemsCourse] = useState(cours.filter(c => c.id_module === module.id_module).map(c => ({
+    label: dateFormatting(new Date(c.debut), new Date(c.fin)),
+    value: c.id_cours
+  })));
+
+  const [openPeriod, setOpenPeriod] = useState(false);
+  const [valuePeriod, setValuePeriod] = useState(formData.periode);
+  const [itemsPeriod, setItemsPeriod] = useState([
+    { label: 'Semestre 1', value: 'Semestre_1' },
+    { label: 'Semestre 2', value: 'Semestre_2' },
+    { label: 'Semestre 3', value: 'Semestre_3' },
+    { label: 'Semestre 4', value: 'Semestre_4' },
+    { label: 'Semestre 5', value: 'Semestre_5' },
+    { label: 'Semestre 6', value: 'Semestre_6' },
+  ]);
+
   const handleSubmit = () => {
-    console.log('Submitting grades:', formData);
+    setFormData({ ...formData, id_cours: valueCourse, periode: valuePeriod });
+    if (!formData.libelle) {
+      Alert.alert("Le nom de l'évaluation est requis");
+      return;
+    }
+    if (!formData.id_module || formData.id_module === 0) {
+      Alert.alert('Le module est requis');
+      return;
+    }
+    if (!formData.id_cours || formData.id_cours === 0) {
+      Alert.alert('Le cours est requis');
+      return;
+    }
+    if (!formData.periode) {
+      Alert.alert('La période est requise');
+      return;
+    }
+
+    onSubmit(formData);
     onClose();
   };
+
+  const renderItem = ({ item }: { item: { type: string } }) => {
+    switch (item.type) {
+      case 'module':
+        return (
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Module sélectionné</Text>
+            <View style={styles.input}>
+              <Text>{module.libelle ?? 'Aucun module sélectionné'}</Text>
+            </View>
+          </View>
+        );
+      case 'libelle':
+        return (
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Nom de l'évaluation</Text>
+            <TextInput
+              style={styles.input}
+              value={formData.libelle}
+              onChangeText={text => setFormData({ ...formData, libelle: text })}
+            />
+          </View>
+        );
+      case 'notemaximale':
+        return (
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Note maximale</Text>
+            <TextInput
+              style={styles.input}
+              value={formData.notemaximale.toString()}
+              onChangeText={text => setFormData({ ...formData, notemaximale: parseInt(text) })}
+              keyboardType="numeric"
+            />
+          </View>
+        );
+      case 'coefficient':
+        return (
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Coefficient</Text>
+            <TextInput
+              style={styles.input}
+              value={formData.coefficient.toString()}
+              onChangeText={text => setFormData({ ...formData, coefficient: parseInt(text) })}
+              keyboardType="numeric"
+            />
+          </View>
+        );
+      case 'cours':
+        return (
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Cours</Text>
+            <DropDownPicker
+              open={openCourse}
+              value={valueCourse}
+              items={itemsCourse}
+              setOpen={setOpenCourse}
+              setValue={setValueCourse}
+              setItems={setItemsCourse}
+              placeholder='Sélectionner un cours'
+            />
+          </View>
+        );
+      case 'periode':
+        return (
+          <View style={styles.formGroup}>
+            <Text style={styles.label}>Période</Text>
+            <DropDownPicker
+              open={openPeriod}
+              value={valuePeriod}
+              items={itemsPeriod}
+              setOpen={setOpenPeriod}
+              setValue={setValuePeriod}
+              setItems={setItemsPeriod}
+              placeholder='Sélectionner une période'
+            />
+          </View>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const data = [
+    { type: 'module' },
+    { type: 'libelle' },
+    { type: 'cours' },
+    { type: 'coefficient' },
+    { type: 'notemaximale' },
+    { type: 'periode' }
+  ];
 
   return (
     <Modal visible={isOpen} transparent animationType="slide">
@@ -37,78 +153,49 @@ export default function MobileAddGradeModal({
         <View style={styles.modalContent}>
           <View style={styles.modalHeader}>
             <Text style={styles.modalTitle}>Nouvelle évaluation</Text>
-            <Button onPress={onClose} title="×" />
+            <TouchableOpacity onPress={onClose}>
+              <FontAwesome6 name="x" size={20} color="gray" />
+            </TouchableOpacity>
           </View>
-
-          <ScrollView style={styles.formContainer}>
-            <View style={styles.formGroup}>
-              <Text style={styles.label}>Module</Text>
-              {/* <Picker
-                selectedValue={formData.moduleId}
-                onValueChange={itemValue =>
-                  setFormData({...formData, moduleId: itemValue})
-                }
-                // style={styles.picker}
-              >
-                {modules.map(module => (
-                  <Picker.Item
-                    key={module.id}
-                    label={module.name}
-                    value={module.id}
-                  />
-                ))}
-              </Picker> */}
-            </View>
-
-            <View style={styles.formGroup}>
-              <Text style={styles.label}>Nom de l'évaluation</Text>
-              <TextInput
-                style={styles.input}
-                value={formData.name}
-                onChangeText={text => setFormData({...formData, name: text})}
-              />
-            </View>
-
-            <View style={styles.formGroup}>
-              <Text style={styles.label}>Date</Text>
-              <TextInput
-                style={styles.input}
-                value={formData.date}
-                onChangeText={text => setFormData({...formData, date: text})}
-              />
-            </View>
-
-            <View style={styles.formGroup}>
-              <Text style={styles.label}>Note maximale</Text>
-              <TextInput
-                style={styles.input}
-                value={formData.maxValue.toString()}
-                onChangeText={text =>
-                  setFormData({...formData, maxValue: parseInt(text)})
-                }
-                keyboardType="numeric"
-              />
-            </View>
-
-            <View style={styles.formGroup}>
-              <Text style={styles.label}>Coefficient</Text>
-              <TextInput
-                style={styles.input}
-                value={formData.coefficient.toString()}
-                onChangeText={text =>
-                  setFormData({...formData, coefficient: parseFloat(text)})
-                }
-                keyboardType="numeric"
-              />
-            </View>
-          </ScrollView>
-
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item, index) => index.toString()}
+            contentContainerStyle={{ paddingBottom: 20, flexGrow: 1 }}  // Ensures that the FlatList expands
+          />
           <View style={styles.modalFooter}>
-            <Button onPress={onClose} title="Annuler" />
-            <Button onPress={handleSubmit} title="Enregistrer" />
+            <TouchableOpacity style={customStyles.cancelButton} onPress={onClose}>
+              <Text style={customStyles.cancelButtonText}>Annuler</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={customStyles.submitButton} onPress={handleSubmit}>
+              <Text style={customStyles.submitButtonText}>Enregistrer</Text>
+            </TouchableOpacity>
+            {/* <Button onPress={onClose} title="Annuler" style={styles.cancelButton} /> */}
+            {/* <Button onPress={handleSubmit} title="Enregistrer" style={styles.saveButton} /> */}
           </View>
         </View>
       </View>
     </Modal>
   );
+}
+
+const customStyles = {
+  cancelButton: {
+    backgroundColor: '#E0E0E0',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 5,
+  },
+  cancelButtonText: {
+    color: '#2C3E50',
+  },
+  submitButton: {
+    backgroundColor: '#2E3494',
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 5,
+  },
+  submitButtonText: {
+    color: 'white',
+  },
 }
--- packages/shared/src/pages/Grades/GradesManagement/ClassGrades.tsx
+++ packages/shared/src/pages/Grades/GradesManagement/ClassGrades.tsx
@@ -1,4 +1,3 @@
-import { Plus } from 'lucide-react'
 import { useEffect, useState } from 'react'
 import {
   Platform,
@@ -12,20 +11,26 @@ import { AddGradeModal } from '../../../components/Grades/GradesManagement/AddGr
 import { styles } from '../../../styles/Grades/GradesManagement/GradesStyles'
 import WebEditNoteModal from '../../../components/Grades/GradesManagement/WebEditNoteModal'
 import WebDeleteNoteModal from '../../../components/Grades/GradesManagement/WebDeleteNoteModal'
-import { API_URL, ClassGradesEvaluation, ClassGradesModule, ClassGradesNote, periodeLabels } from '../../../types/types'
+import { API_URL, ClassGradesEvaluation, ClassGradesModule, ClassGradesNote, FormEvaluation, periodeLabels } from '../../../types/types'
 import { useEditDeleteLoader } from '../../../components/Button/EditDeleteLoader'
-import { dateFormatting, getTime } from '../../../utils/stringUtils'
-import { fetchClassGrades } from '@shared/backend/services/classgrades'
+// import { dateFormatting, getTime } from '../../../utils/stringUtils'
+import { fetchClassGrades } from '../../../backend/services/classgrades'
 
 import WebDeleteGradeModal from '../../../components/Grades/GradesManagement/WebDeleteGradeModal'
+import { PlusButton } from '../../../components/Button/PlusButton'
+import { dateFormatting } from '../../../utils/stringUtils'
 
 export function ClassGrades() {
   const { Edit, Delete } = useEditDeleteLoader()
 
+  const [showAddGradeModal, setShowAddGradeModal] = useState(false)
   const [showNoteForm, setShowNoteForm] = useState(false)
-  const [AddNoteModal, setAddNoteModal] = useState<any>(null)
+
   const [NoteForm, setNoteForm] = useState<any>(null)
-  const [showAddGrade, setShowAddGrade] = useState(false)
+
+  const [selectedModule, setSelectedModule] = useState<ClassGradesModule | null>(null)
+
+
   const [searchQuery, setSearchQuery] = useState('')
   const [modulesEvalNotes, setModuleEvalNotes] = useState<ClassGradesModule[]>([])
   const [cours, setCours] = useState<any>(null)
@@ -34,8 +39,6 @@ export function ClassGrades() {
   const [showEditNote, setShowEditNote] = useState(false);
   const [showDeleteNote, setShowDeleteNote] = useState(false);
   const [showDeleteEvaluation, setShowDeleteEvaluation] = useState(false);
-
-
 
   useEffect(() => {
     fetchClassGrades()
@@ -69,32 +72,99 @@ export function ClassGrades() {
   useEffect(() => {
     const loadComponents = async () => {
       if (Platform.OS === 'web') {
-        const { AddNoteModal } = await import(
-          '../../../../../web/src/components/Grades/Manage/AddNoteModal.web'
-        )
-        const { NoteForm } = await import(
-          '../../../../../web/src/components/Grades/Manage/NoteForm.web'
-        )
-        setAddNoteModal(() => AddNoteModal)
+        const { NoteForm } = await import('../../../../../web/src/components/Grades/Manage/NoteForm.web')
         setNoteForm(() => NoteForm)
       } else {
-
+        const { NoteForm } = await import('../../../../../mobile/src/components/Grades/GradesManagement/NoteForm.native')
+        setNoteForm(() => NoteForm)
       }
     }
 
     loadComponents().then(r => r)
   }, [])
 
-  const handleAddGrade = (module: ClassGradesModule) => {
-    // setSelectedModule(module);
-    setShowAddGrade(true);
-  };
+  if (!Edit || !Delete) return null;
 
-  const filteredModules = modulesEvalNotes.filter(
+  const filteredGradeModules = modulesEvalNotes.filter(
     (module) =>
       module.libelle.toLowerCase().includes(searchQuery.toLowerCase()) ||
       module.codeapogee.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
+  const handleAddGrade = (module: ClassGradesModule) => {
+    setSelectedModule(module);
+    setShowAddGradeModal(true);
+  }
+
+  const handleSubmitGrade = async (data: FormEvaluation) => {
+    try {
+      const response = await fetch(`${API_URL}/api/classgrades/insert-evaluation`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data),
+      })
+
+      if (!response.ok) throw new Error('Erreur réseau')
+
+      const result = await response.json()
+      const actualCourse = cours.find((c: any) => c.id_cours === data.id_cours)
+      const date = dateFormatting(new Date(actualCourse.debut), new Date(actualCourse.fin))
+      const newEvaluation: ClassGradesEvaluation = {
+        id_eval: result.id_eval,
+        libelle: data.libelle,
+        periode: data.periode,
+        date: date,
+        notemaximale: data.notemaximale,
+        coefficient: data.coefficient,
+        notes: [],
+      }
+      setModuleEvalNotes((prev) => {
+        const newModules = prev.map((m) => {
+          if (m.id_module === data.id_module) {
+            return { ...m, evaluations: [...m.evaluations, newEvaluation] }
+          }
+          return m
+        })
+        return newModules
+      })
+    } catch (error: any) {
+      console.error('Erreur:', error)
+    }
+
+    setShowAddGradeModal(false)
+    setSelectedModule(null)
+  }
+
+  const handleDeleteGrade = async (evaluation: ClassGradesEvaluation) => {
+    try {
+      const response = await fetch(`${API_URL}/api/classgrades/delete-evaluation/${evaluation.id_eval}`, {
+        method: 'DELETE',
+      })
+
+      if (!response.ok) throw new Error('Erreur réseau')
+
+      setModuleEvalNotes((prev) => {
+        const newModules = prev.map((m) => {
+          if (m.evaluations.find((e) => e.id_eval === evaluation.id_eval)) {
+            return {
+              ...m,
+              evaluations: m.evaluations.filter((e) => e.id_eval !== evaluation.id_eval),
+            }
+          }
+          return m
+        })
+        return newModules
+      })
+     
+    } catch (error: any) {
+      console.error('Erreur:', error)
+    }
+  }
+
+  /* const handleAddGrade = (module: ClassGradesModule) => {
+    // setSelectedModule(module);
+    setShowAddGrade(true);
+  }; */
 
   const calculateEvaluationAverage = (evaluation: ClassGradesEvaluation) => {
     if (evaluation.notes.length === 0) return ''
@@ -124,17 +194,17 @@ export function ClassGrades() {
     // setShowAddNote(true); // Ouvrir le modal d'ajout de note
     setShowNoteForm(true); // Ouvrir le formulaire de note
   };
-
+ 
   const handleEditNote = (note: ClassGradesNote) => {
     setSelectedNote(note);
     setShowEditNote(true);
   };
-
+ 
   const handleDeleteNote = (note: ClassGradesNote) => {
     setSelectedNote(note);
     setShowDeleteNote(true);
   };
-
+ 
   const handleAddNote = (evaluation: Evaluation) => {
     setSelectedEvaluation(evaluation) 
     setShowAddNote(true) 
@@ -172,81 +242,81 @@ export function ClassGrades() {
     setShowDeleteEvaluation(true);
   }; */
 
-
-
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        {/* <TouchableOpacity onPress={handleAddGrade} style={styles.addButton}>
-          <Plus className="w-4 h-4" />
-        </TouchableOpacity> */}
-        {/* <TouchableOpacity onPress={handleAddGrade} style={styles.addButton}>
-          <Plus className="w-4 h-4" />
-        </TouchableOpacity> */}
-      </View>
-
       <View style={styles.searchContainer}>
         <TextInput
-          value={searchQuery}
-          onChangeText={setSearchQuery}
+          onChangeText={(text) => setSearchQuery(text)}
           placeholder="Rechercher un module..."
           style={styles.searchInput}
         />
 
       </View>
       <ScrollView>
-        {filteredModules.map((module) => (
-          module.evaluations.length > 0 && (
-            <View key={module.id_module} style={styles.moduleCard}>
-              <View style={styles.moduleHeader}>
-                <Text style={styles.moduleTitle}>{module.libelle}</Text>
-              </View>
+        {filteredGradeModules.map((module) => (
+          <View key={module.id_module} style={styles.moduleCard}>
+            <View style={styles.moduleHeader}>
+              <Text style={styles.moduleTitle}>
+                {module.libelle} {module.codeapogee !== null && `(${module.codeapogee}) `}
+                <TouchableOpacity onPress={() => handleAddGrade(module)} style={{ marginLeft: 8 }}>
+                  <PlusButton />
+                </TouchableOpacity>
+              </Text>
+            </View>
 
-              {module.evaluations.length > 0 &&
-                module.evaluations.map((e: ClassGradesEvaluation) => (
-                  <View key={e.id_eval} style={styles.evaluationCard}>
-                    <View style={styles.evaluationHeader}>
-                      <Text style={styles.evaluationTitle}>
-                        {e.libelle}
-                        {/* <TouchableOpacity onPress={() => handleAddNote(e)} style={styles.addButton}>
-                          <Plus className="w-4 h-4" />
-                        </TouchableOpacity> */}
-                      </Text>
-                      <Text style={styles.evaluationSubtitle}>
-                        Période de l'évaluation :{' ' + periodeLabels[e.periode]} -{' '}
-                        Date : {e.date} - Coefficient : {e.coefficient} {calculateEvaluationAverage(e)}
-                      </Text>
-                    </View>
-                    <View key={'m' + module.id_module + 'e' + e.id_eval} style={styles.table}>
-                      {/* <View style={styles.tableRow}>
-                        <Text style={styles.tableCell}>Période de l'évaluation : {periodeLabels[e.periode]}</Text>
-                        <Text style={styles.tableCell}>Date : {e.date}</Text>
-                        <Text style={styles.tableCell}>Coefficient : {e.coefficient}</Text>
-                        <Text style={styles.tableCell}>Moyenne : {calculateEvaluationAverage(e)}</Text>
-                      </View> */}
-                      {e.notes.map((n: ClassGradesNote) => (
-                        <View key={'m' + module.id_module + 'e' + e.id_eval + 'u' + n.numero_etudiant} style={styles.tableRow}>
-                          <Text style={styles.tableCell}>{n.numero_etudiant}</Text>
-                          <Text style={styles.tableCell}>{n.nom + ' ' + n.prenom}</Text>
-                          <Text style={styles.tableCell}>{n.note !== null ? `${n.note}/${e.notemaximale}` : '-'}</Text>
-                          <Text style={styles.tableCell}>{n.commentaire}</Text>
-                          {/*
+            {module.evaluations.length > 0 &&
+              module.evaluations.map((e: ClassGradesEvaluation) => (
+                <View key={e.id_eval} style={styles.evaluationCard}>
+                  <View style={styles.evaluationHeader}>
+                    <Text style={styles.evaluationTitle}>
+                      {e.libelle} {''}
+                      <TouchableOpacity onPress={() => setShowNoteForm(true)} style={{ marginLeft: 10 }}>
+                        <PlusButton />
+                      </TouchableOpacity>
+                      <Delete onDelete={() => handleDeleteGrade(e)} confirmMessage={`Voulez-vous supprimer l'évaluation ${e.libelle} du ${e.periode.replace('_', ' ')}. Attention cette action supprimera toutes les notes de l'évaluation !`} />
+                    </Text>
+                    <Text style={styles.evaluationSubtitle}>
+                      Période de l'évaluation :{' ' + periodeLabels[e.periode]} -{' '}
+                      Date : {e.date} - Coefficient : {e.coefficient} {calculateEvaluationAverage(e)}
+                    </Text>
+                  </View>
+                  <View key={'m' + module.id_module + 'e' + e.id_eval} style={styles.table}>
+                    {e.notes.map((n: ClassGradesNote) => (
+                      <View key={'m' + module.id_module + 'e' + e.id_eval + 'u' + n.numero_etudiant} style={styles.tableRow}>
+                        <Text style={styles.tableCell}>{n.numero_etudiant}</Text>
+                        <Text style={styles.tableCell}>{n.nom + ' ' + n.prenom}</Text>
+                        <Text style={styles.tableCell}>{n.note !== null ? `${n.note}/${e.notemaximale}` : '-'}</Text>
+                        <Text style={styles.tableCell}>{n.commentaire}</Text>
+                        {/*
                           <Text style={styles.tableCell}>
                             <Text style={styles.gradeStatus}>
                             'Publiée'
                             </Text>
                           </Text> 
                           */}
-                          {/* <Edit onEdit={() => handleEditNote(n)} /> */}
-                          {/* <Delete onDelete={() => handleDeleteNote(n)} /> */}
-                        </View>
-                      ))}
-                    </View>
+                        {/* <Edit onEdit={() => handleEditNote(n)} /> */}
+                        {/* <Delete onDelete={() => handleDeleteNote(n)} /> */}
+                      </View>
+                    ))}
                   </View>
-                ))}
-            </View>
-          )))}
+                </View>
+              ))}
+          </View>
+        ))}
       </ScrollView>
+
+      {showAddGradeModal && selectedModule && (
+        <AddGradeModal
+          isOpen={showAddGradeModal}
+          onClose={() => {
+            setShowAddGradeModal(false)
+            setSelectedModule(null)
+          }}
+          onSubmit={handleSubmitGrade}
+          module={selectedModule}
+          cours={cours}
+        />
+      )}
     </View>
   )
 }
