fd4f0d6e26d179788a33c7c54fb77a17ac870ca1
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-02-27 14:45:19 +0100

    Ajout du composant PlusButton pour les plateformes mobile et web, avec chargement dynamique selon la plateforme.

---
+++ packages/mobile/src/components/Button/PlusButton.native.tsx
@@ -0,0 +1,12 @@
+import Feather from 'react-native-vector-icons/Feather'
+import { TouchableOpacity } from "react-native"
+
+const iconButton = {
+  marginLeft: 8,
+}
+
+export default function PlusButton() {
+  return (
+    <Feather name="plus" size={16} color="#2E3494" />
+  )
+}
