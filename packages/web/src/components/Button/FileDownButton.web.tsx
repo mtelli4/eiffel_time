3a8d79f2b46c9787c8181e198c2886482979372a
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-07 15:23:01 +0100

    Refactor FileDownButton component to allow dynamic color and size props

--- packages/web/src/components/Button/FileDownButton.web.tsx
+++ packages/web/src/components/Button/FileDownButton.web.tsx
@@ -7,7 +7,7 @@ interface PlusButtonProps {
 
 export default function PlusButton({ color = "#2E3494", size = 4 }: PlusButtonProps) {
   return (
-    <div className={`text-red transition-colors`}>
+    <div className={`text-${color} transition-colors`}>
       <FileDown className={`w-${size} h-${size} text-${color}`} />
     </div>
   )
