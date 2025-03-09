c07b6b23ffffa023a02c313095b87c1d02908281
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-09 12:26:11 +0100

    refactor: remove unused MMKVStorage import and initialization in UserContext

--- packages/mobile/src/context/UserContext.js
+++ packages/mobile/src/context/UserContext.js
@@ -1,14 +1,9 @@
 import React, {createContext, useEffect, useState} from 'react';
-// import { storage } from '../storage/storage';
-import MMKVStorage from "react-native-mmkv-storage";
+import { storage } from '../storage/storage';
 
 // Crée le contexte
 export const UserContext = createContext();
 
-const storage = new MMKVStorage.Loader()
-  .withInstanceID("userStorage")
-  .initialize();
-  
 // Fournisseur du contexte
 export const UserProvider = ({children}) => {
   // const [user, setUser] = useState({}); // Rôle par défaut
