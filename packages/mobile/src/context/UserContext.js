bf685bcd5f12cf15fcea1ceafe711b8125760c72
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-03-09 12:24:49 +0100

    feat: add react-native-mmkv-storage for improved user data management

--- packages/mobile/src/context/UserContext.js
+++ packages/mobile/src/context/UserContext.js
@@ -1,9 +1,14 @@
 import React, {createContext, useEffect, useState} from 'react';
-import { storage } from '../storage/storage';
+// import { storage } from '../storage/storage';
+import MMKVStorage from "react-native-mmkv-storage";
 
 // Crée le contexte
 export const UserContext = createContext();
 
+const storage = new MMKVStorage.Loader()
+  .withInstanceID("userStorage")
+  .initialize();
+  
 // Fournisseur du contexte
 export const UserProvider = ({children}) => {
   // const [user, setUser] = useState({}); // Rôle par défaut
